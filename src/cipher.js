8eb8d397ed854bca1a9cf812b83174ec5ed3a25e
natachang <nataliechangch99@gmail.com>
2018-05-24 18:06:37 -0500

    proyecto

--- src/cipher.js
+++ src/cipher.js
@@ -1,3 +1,18 @@
-window.cipher = {
-  // ... 
-};
+/*window.cipher = {
+   
+  encode : 
+  function(displa,text){
+    var output="";
+    for (var i=0; i<text.length;i++){
+      output += String.fromCharCode(text.charCodeAt(i)+displa);
+    }
+  
+    document.getElementById("mes2").value = output;
+   
+    
+  }
+/*
+  decode = function(displa,text){
+
+  }
+*/
---
+++ src/index.js
@@ -0,0 +1,21 @@
+function cipherEncode(){
+	var output="";
+	var text = document.getElementById("mes").value;
+	var displa = parseInt(document.getElementById("disp").value);
+	for (var i=0; i<text.length;i++){
+		output += String.fromCharCode(text.charCodeAt(i)+displa);
+    }
+    //return alert(output); 
+    document.getElementById("mes2").value = output;
+}
+
+function cipherDecode(){
+    var output="";
+	var text = document.getElementById("mes").value;
+	var displa = parseInt(document.getElementById("disp").value);
+	for (var i=0; i<text.length;i++){
+		output += String.fromCharCode(text.charCodeAt(i)+displa);
+    }
+    //return alert(output); 
+    document.getElementById("mes2").value = output;
+}
