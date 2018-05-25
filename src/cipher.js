d45773d4c4a7b9ad8c7b8d09a1047f35dc50a982
natachang <nataliechangch99@gmail.com>
2018-05-25 16:32:57 -0500

    proyecto

--- src/cipher.js
+++ src/cipher.js
@@ -1,18 +1,25 @@
-/*window.cipher = {
+  /*window.cipher = {
    
   encode : 
-  function(displa,text){
-    var output="";
-    for (var i=0; i<text.length;i++){
-      output += String.fromCharCode(text.charCodeAt(i)+displa);
+  function cipherEncode(){
+	var output="";
+	var text = document.getElementById("mes").value;
+	var displa = parseInt(document.getElementById("disp").value);
+	for (var i=0; i<text.length;i++){
+		output += String.fromCharCode(text.charCodeAt(i)+displa);
     }
-  
+    //return alert(output); 
     document.getElementById("mes2").value = output;
-   
-    
   }
-/*
-  decode = function(displa,text){
 
+  decode : 
+   function cipherDecode(){
+   var output="";
+	 var text = document.getElementById("mes").value;
+	 var displa = parseInt(document.getElementById("disp").value);
+	 for (var i=0; i<text.length;i++){
+		output += String.fromCharCode(text.charCodeAt(i)-displa);
+    }
+    //return alert(output); 
+    document.getElementById("mes2").value = output;
   }
-*/
--- src/index.js
+++ src/index.js
@@ -1,21 +1,36 @@
-function cipherEncode(){
-	var output="";
-	var text = document.getElementById("mes").value;
-	var displa = parseInt(document.getElementById("disp").value);
-	for (var i=0; i<text.length;i++){
-		output += String.fromCharCode(text.charCodeAt(i)+displa);
-    }
-    //return alert(output); 
-    document.getElementById("mes2").value = output;
-}
 
-function cipherDecode(){
-    var output="";
-	var text = document.getElementById("mes").value;
-	var displa = parseInt(document.getElementById("disp").value);
-	for (var i=0; i<text.length;i++){
-		output += String.fromCharCode(text.charCodeAt(i)-displa);
-    }
-    //return alert(output); 
-    document.getElementById("mes2").value = output;
-}
+
+    (function(){
+    
+
+    document.getElementById("cip");
+    cip.addEventListener("click",   function cipherEncode(){
+        var output="";
+        var text = document.getElementById("mes").value;
+        var displa = parseInt(document.getElementById("disp").value);
+        for (var i=0; i<text.length;i++){
+            output += String.fromCharCode(text.charCodeAt(i)+displa);
+        }
+        //return alert(output); 
+        document.getElementById("mes2").value = output;
+      });
+      
+    }())
+
+ 
+    (function(){
+    
+
+        document.getElementById("dec");
+        dec.addEventListener("click",   function cipherDecode(){
+            var output="";
+            var text = document.getElementById("mes").value;
+            var displa = parseInt(document.getElementById("disp").value);
+            for (var i=0; i<text.length;i++){
+               output += String.fromCharCode(text.charCodeAt(i)-displa);
+           }
+           //return alert(output); 
+           document.getElementById("mes2").value = output;
+         });
+          
+        }())
